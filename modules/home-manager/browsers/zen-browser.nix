39e11eb10011907ebbfec24ac84558f3a483243d
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-27 23:34:04 +0000

    fix: update zen-browser profiles structure and extension packages mapping

--- modules/home-manager/browsers/zen-browser.nix
+++ modules/home-manager/browsers/zen-browser.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, pkgs, ... }:
 {
   imports = [
     inputs.zen-browser.homeModules.beta
@@ -6,20 +6,25 @@
 
   programs.zen-browser.enable = true;
 
-  programs.zen-browser.profiles.*.extensions.packages = 
-     with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
-          adnauseam
-          violentmonkey
-          darkreader
-          bitwarden
-          #TorBox Control missing
-          duckduckgo-privacy-essentials
-          wayback-machine
-          user-agent-string-switcher
-          # Faster Pageload missing
-          # Translate this page missing
-          foxyproxy-standard
-          clearurls
-          # NopeCHA missing
-        ];
+  programs.zen-browser.profiles = lib.attrsets.mapAttrs (name: value: {
+    extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+      adnauseam
+      violentmonkey
+      darkreader
+      bitwarden
+      #TorBox Control missing
+      duckduckgo-privacy-essentials
+      wayback-machine
+      user-agent-string-switcher
+      # Faster Pageload missing
+      # Translate this page missing
+      foxyproxy-standard
+      clearurls
+      # NopeCHA missing
+    ];
+  }) {
+    # This will apply the extensions to a profile named "default"
+    # and any other profiles you might define here.
+    default = {};
+  };
 }
