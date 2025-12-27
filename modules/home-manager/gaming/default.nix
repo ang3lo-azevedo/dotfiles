7dda608ed7d3b45354fc290bd5e6dd0a6672e560
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-27 21:41:35 +0000

    chore: stage all pending changes

--- modules/home-manager/gaming/default.nix
+++ modules/home-manager/gaming/default.nix
@@ -1,20 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Gaming related configurations
   # VR support imported from a separate file
   imports = [
     ./vr.nix
   ];
-
-  # Enable Steam
-  programs.steam = {
-    enable = true;
-
-    # Add the CachyOS Proton
-    extraCompatPackages = [
-      inputs.nix-proton-cachyos.packages.${system}.proton-cachyos
-    ];
-  };
 
   home.packages = with pkgs; [
     lutris
