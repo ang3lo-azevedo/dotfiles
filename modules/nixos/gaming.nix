b4b010a1c39348aba579c3e8a2ad4db93bd5d0f9
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-27 21:41:05 +0000

    Fix module paths and add submodules

---
+++ modules/nixos/gaming.nix
@@ -0,0 +1,12 @@
+{ inputs, pkgs, ... }:
+{
+  # Enable Steam at the system level
+  programs.steam = {
+    enable = true;
+
+    # Add the CachyOS Proton
+    extraCompatPackages = [
+      inputs.nix-proton-cachyos.packages.${pkgs.system}.proton-cachyos
+    ];
+  };
+}
