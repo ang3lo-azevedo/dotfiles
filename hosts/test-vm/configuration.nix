46d6d6d58b30c630b310f301fce1c7782f4f6aba
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-28 11:50:16 +0000

    feat: update test VM configuration with necessary imports and hostname definition

--- hosts/test-vm/configuration.nix
+++ hosts/test-vm/configuration.nix
@@ -1,6 +1,13 @@
 { ... }:
 {
   imports = [
-    ../pc-angelo/configuration.nix
+    ./hardware-configuration.nix
+    ../../modules/nixos/common.nix
+    ../../modules/nixos/gaming.nix
+    ../../modules/nixos/virtualisation.nix
   ];
+
+  networking.hostName = "pc-angelo"; # Define your hostname.
+  
+  services.displayManager.ly.enable = true;
 }
