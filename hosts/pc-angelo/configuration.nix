51701f9bd981414cf4d98a9d5b5066e2884a2902
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-28 19:22:29 +0000

    fix(configuration.nix): add LUKS container support and Btrfs filesystem configuration

--- hosts/pc-angelo/configuration.nix
+++ hosts/pc-angelo/configuration.nix
@@ -7,6 +7,16 @@
     ../../modules/nixos/virtualisation.nix
   ];
 
+  # Ensure the system knows to open the LUKS container
+  boot.initrd.luks.devices."cryptroot" = {
+    device = "/dev/disk/by-partlabel/disk-main-luks";
+    allowDiscards = true;
+  };
+
+  # Required for Btrfs on LVM on LUKS
+  boot.initrd.supportedFilesystems = [ "btrfs" ];
+  boot.initrd.availableKernelModules = [ "virtio_pci" "virtio_blk" "virtio_scsi" "ata_piix" "uhci_hcd" ];
+
   networking.hostName = "pc-angelo"; # Define your hostname.
   
   services.displayManager.ly.enable = true;
