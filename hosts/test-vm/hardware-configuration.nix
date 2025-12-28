26e193e31aa442f2812695f6d898836c96fd1b95
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-28 12:06:30 +0000

    feat(vm): remove unnecessary imports from hardware-configuration.nix

--- hosts/test-vm/hardware-configuration.nix
+++ hosts/test-vm/hardware-configuration.nix
@@ -4,10 +4,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/profiles/quemu-guest.nix")
-    ];
-
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
