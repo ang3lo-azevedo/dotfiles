099df11b66ccb08ecb9a1978bc6e790e9e1114a7
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 11:00:40 +0000

    fix(secrets): correct test-vm public key format in secrets configuration

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   yubikey = "age1yubikey1qgy9lcjxxsyl8z3daz34a4504v9fc2fv20w5784qwhlaz40chms7gjer68d";
-  test-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbZ+K6BHi+CWPLTWvt9Hhu3ZZNouVffIfDOSEq5IOiw";
+  test-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbZ+K6BHi+CWPLTWvt9Hhu32ZNouVffIfDOSEq5IOiw";
 in
 {
   "root_password.age".publicKeys = [ yubikey test-vm ];
