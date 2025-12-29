b3d3aa4af915ed42cb08ee0b3accfe71e15ed4fc
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 18:55:34 +0000

    fix(configuration): update autostart script and fix file paths in configuration

--- modules/home-manager/window-manager/waybar.nix
+++ modules/home-manager/window-manager/waybar.nix
@@ -2,4 +2,5 @@
   programs.waybar.enable = true;
 
   xdg.configFile."waybar/config".source = ../../../home/ang3lo/config/waybar/config;
+  xdg.configFile."waybar/style.css".source = ../../../home/ang3lo/config/waybar/style.css;
 }
