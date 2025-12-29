aca448bba60d7ab400f1ab2610dd44b8b60c1027
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 12:42:20 +0000

    fix(secrets): update root and user password configurations with new keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
   yubikey = "age1yubikey1qgy9lcjxxsyl8z3daz34a4504v9fc2fv20w5784qwhlaz40chms7gjer68d";
 
+  pc-angelo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHITd9574ykchPZZUfm/wvC5E2aF1Ay3pBIyXrEar8Q";
+
   users = [ yubikey ];
 
-  systems = [ ];
+  systems = [ pc-angelo ];
 
   usersAndSystems = users ++ systems;
 in
