a2f303919e20d92d35f660356bc1d466155e836a
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 00:32:52 +0000

    fix(secrets): remove unused systems variable from secrets configuration

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,6 @@
 let
   yubikey = "age1yubikey1qgy9lcjxxsyl8z3daz34a4504v9fc2fv20w5784qwhlaz40chms7gjer68d";
   test-vm = "age1j2yrgva9t5553jl0j2s766x48jna2jferhhfz0hkk0jmenm7n3aqgwd7sx";
-  
-  systems = [ test-vm ];
 in
 {
   "root_password.age".publicKeys = [ yubikey test-vm ];
