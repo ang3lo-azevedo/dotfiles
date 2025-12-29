af1ad0aa086a1f4cc23ff9378c9de0f0e77e3e6f
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 22:01:22 +0000

    feat(mango): update key bindings and enable swaync service

--- modules/home-manager/window-manager/swaync.nix
+++ modules/home-manager/window-manager/swaync.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 let
   # Path (relative to this file) to the repo directory containing swaync configs
   swayncDir = ../../../home/ang3lo/.config/swaync;
@@ -5,7 +6,9 @@ in
 {
   services.swaync.enable = true;
 
-  xdg.configFile."swaync/config.json".source = "${swayncDir}/config.json";
+  # Use lib.mkForce so these home-manager `xdg.configFile` entries have
+  # priority over other module definitions and avoid conflicting values.
+  xdg.configFile."swaync/config.json" = lib.mkForce { source = "${swayncDir}/config.json"; };
   xdg.configFile."swaync/style.css".source = "${swayncDir}/style.css";
   xdg.configFile."swaync/widgets.css".source = "${swayncDir}/widgets.css";
 }
