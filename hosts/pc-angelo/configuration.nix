eb4d1a86d989f0b7e492f1397baf563ce6ea365f
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 00:31:00 +0000

    fix(configuration): add SSH host key identification for Agenix

--- hosts/pc-angelo/configuration.nix
+++ hosts/pc-angelo/configuration.nix
@@ -30,6 +30,9 @@
   # Make sure systemd is enabled in initrd for plymouth
   boot.initrd.systemd.enable = true;
 
+  # Identify the SSH host key to be used with Agenix
+  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
   # Provision secrets with Agenix
   age.secrets.user_password.file = ../../secrets/user_password.age;
   age.secrets.root_password.file = ../../secrets/root_password.age;
