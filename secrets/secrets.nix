241224d15c66b7ddde32956bf515a95119fde94b
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 12:11:28 +0000

    fix(secrets): update user and root password configurations and remove test-vm public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,13 @@
 let
   yubikey = "age1yubikey1qgy9lcjxxsyl8z3daz34a4504v9fc2fv20w5784qwhlaz40chms7gjer68d";
-  test-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbZ+K6BHi+CWPLTWvt9Hhu32ZNouVffIfDOSEq5IOiw";
+
+  users = [ yubikey ];
+
+  systems = [ ];
+
+  usersAndSystems = users ++ systems;
 in
 {
-  "root_password.age".publicKeys = [ yubikey test-vm ];
-  "user_password.age".publicKeys = [ yubikey test-vm ];
+  "root_password.age".publicKeys = usersAndSystems;
+  "user_password.age".publicKeys = usersAndSystems;
 }
