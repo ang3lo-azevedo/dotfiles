cb692d6510c1771a549fe535138fc4648c2e40dc
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 21:52:19 +0000

    feat(swaync): update swaync configuration paths and enable service

--- modules/home-manager/window-manager/swaync.nix
+++ modules/home-manager/window-manager/swaync.nix
@@ -3,6 +3,8 @@ let
   swayncDir = ../../../home/ang3lo/.config/swaync;
 in
 {
+  services.swaync.enable = true;
+
   xdg.configFile."swaync/config.json".source = "${swayncDir}/config.json";
   xdg.configFile."swaync/style.css".source = "${swayncDir}/style.css";
   xdg.configFile."swaync/widgets.css".source = "${swayncDir}/widgets.css";
