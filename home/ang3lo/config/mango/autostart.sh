a9a0aecd1ef1cc41019c650555f9c8326d080d8c
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-29 17:40:29 +0000

    fix(configuration): update autostart script and enable autostart in home.nix

--- home/ang3lo/config/mango/autostart.sh
+++ home/ang3lo/config/mango/autostart.sh
@@ -1,26 +1,29 @@
+# Start the terminal
+foot &
+
 # Update DBus environment
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+#dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 # Polkit authentication agent
-/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+#/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 
 # Background
-swaybg -c "#000000" &
+#swaybg -c "#000000" &
 
 # Top bar
-waybar &
+#waybar &
 
 # Notification daemon
-swaync &
+#swaync &
 
 # Idle management
-swayidle -w \
-	timeout 601 'wlr-dpms off' \
-	timeout 600 'swaylock -f' \
-	before-sleep 'swaylock -f' &
+#swayidle -w \
+#	timeout 601 'wlr-dpms off' \
+#	timeout 600 'swaylock -f' \
+#	before-sleep 'swaylock -f' &
 
 # Clipboard history
-wl-paste --watch cliphist store &
+#wl-paste --watch cliphist store &
 
 # Night light
-wlsunset -T 3501 -t 3500 &
+#wlsunset -T 3501 -t 3500 &
