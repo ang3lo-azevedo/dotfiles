7056c93953e8971cc0fc296a1deefeec1dee79ef
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-12-30 17:00:49 +0000

    feat(disko): refactor disk configuration to inherit device and streamline partition definitions

--- hosts/pc-angelo/disko.nix
+++ hosts/pc-angelo/disko.nix
@@ -1,34 +1,34 @@
-{
+{ device ? "/dev/nvme0n1"
+, ...
+}: {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/sda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "umask=0077"
-                ];
-              };
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "umask=0077"
+              ];
             };
-            luks = {
-              size = "-256G"; # Leave 256GB at the end of the disk unallocated for ntfs partition
+          };
+          luks = {
+            size = "-256G"; # Leave 256GB at the end of the disk unallocated for ntfs partition
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              settings.allowDiscards = true;
               content = {
-                type = "luks";
-                name = "cryptroot";
-                settings.allowDiscards = true;
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
-                };
+                type = "lvm_pv";
+                vg = "pool";
               };
             };
           };
