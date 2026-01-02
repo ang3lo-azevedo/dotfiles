502f3625987759eea3032620e5da0e795780b32a
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-02 14:09:22 +0000

    fix: update configuration files and bindings for improved system setup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   yubikey = "age1yubikey1qgy9lcjxxsyl8z3daz34a4504v9fc2fv20w5784qwhlaz40chms7gjer68d";
 
-  pc-angelo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHITd9574ykchPZZUfm/wvC5E2aF1Ay3pBIyXrEar8Q";
+  pc-angelo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKo5FTbsC2Ey5qaRfKkTjrn4S5MRTNjxqzy7ZPObS/F";
 
   users = [ yubikey ];
 
