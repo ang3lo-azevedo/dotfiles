79a339ce6b735921ee6d4f8d6ae779d82161b0e0
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-02 14:09:26 +0000

    fix: update configuration files and scripts for improved system setup

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -2,4 +2,15 @@
   imports = [
     ./vscode.nix
   ];
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name  = "Angelo Azevedo";
+        email = "ang3lo@azevedos.eu.org";
+      };
+      init.defaultBranch = "main";
+    };
+  };
 }
