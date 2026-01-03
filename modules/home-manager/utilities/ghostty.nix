1017c330c78be25d0db339178d9984c8a60f546f
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-03 14:50:37 +0000

    feat: update Zsh utilities; enhance configurations for ghostty and starship

--- modules/home-manager/utilities/ghostty.nix
+++ modules/home-manager/utilities/ghostty.nix
@@ -3,6 +3,11 @@ let
   ghosttyDir = ../../../home/ang3lo/.config/ghostty;
 in
 {
-  programs.ghostty.enable = true;
-  #xdg.configFile."ghostty/config".source = lib.mkForce (ghosttyDir + "/config");
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      font-family = "JetBrainsMono Nerd Font";
+      theme = "catppuccin-mocha";
+    };
+  };
 }
