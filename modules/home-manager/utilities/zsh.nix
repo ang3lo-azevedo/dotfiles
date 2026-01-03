a0b17bbdd87e8c76de97053803a0f428b6b1bf81
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-03 12:21:31 +0000

    feat: update wlsunset script for improved process handling; add kwallet support in Ly display manager configuration

--- modules/home-manager/utilities/zsh.nix
+++ modules/home-manager/utilities/zsh.nix
@@ -1,5 +1,9 @@
 {
   programs.zsh = {
+    enable = true;
     autosuggestion.enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake /home/ang3lo/nix-config";
+    };
   };
 }
