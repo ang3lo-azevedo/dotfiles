07e5083d6c0168a36551439bd8dd5ba121058d09
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-03 15:34:43 +0000

    feat: update window manager configurations; remove wlsunset and swayidle, add service definitions for them

--- modules/home-manager/window-manager/default.nix
+++ modules/home-manager/window-manager/default.nix
@@ -1,15 +1,14 @@
 {
   imports = [
+    ./services
     ./mangowc.nix
     ./waybar.nix
     ./fuzzel.nix
     ./swaybg.nix
     ./swaync.nix
-    ./wlsunset.nix
     ./iwmenu.nix
     ./pwmenu.nix
     ./bzmenu.nix
-    ./swayidle.nix
     ./swaylock.nix
     ./wl-clipboard.nix
   ];
--- modules/home-manager/window-manager/wlsunset.nix
+++
@@ -1,3 +0,0 @@
-{
-  programs.wlsunset.enable = true; 
-}
