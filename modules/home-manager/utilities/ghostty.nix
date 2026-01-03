497f44428d500b3cecedfb1dac75a143b9449048
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-03 15:25:32 +0000

    feat: update configuration files; add apple-emoji and treefmt-nix, remove ghostty config, and enhance theming and utilities

--- modules/home-manager/utilities/ghostty.nix
+++ modules/home-manager/utilities/ghostty.nix
@@ -1,12 +1,12 @@
-{ lib, ... }:
-let
-  ghosttyDir = ../../../home/ang3lo/.config/ghostty;
-in
 {
   programs.ghostty = {
     enable = true;
     settings = {
-      font-family = "JetBrainsMono Nerd Font";
+      #font-family = "JetBrainsMono Nerd Font";
+      window-decoration = "none";
+      window-padding-y = "12,12";
+      window-padding-x = "12,12";
+      window-padding-balance = true;
     };
   };
 }
