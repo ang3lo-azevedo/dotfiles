ebde83ea40bc89786c84bcf951e2522bdf73034c
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-04 16:19:27 +0000

    feat: refactor Zen Browser extension imports and reintroduce AdNauseam with updated settings

--- modules/home-manager/browsers/zen-browser/extensions/default.nix
+++ modules/home-manager/browsers/zen-browser/extensions/default.nix
@@ -1,17 +1,13 @@
+{ lib, ... }:
+
+let
+  # Get all .nix files in the current directory
+  files = builtins.attrNames (builtins.readDir ./.);
+  # Filter out default.nix and keep only .nix files
+  nixFiles = builtins.filter (name: name != "default.nix" && lib.hasSuffix ".nix" name) files;
+  # Create paths for imports
+  importsList = map (name: ./. + "/${name}") nixFiles;
+in
 {
-  imports = [
-    ./violentmonkey.nix
-    ./darkreader.nix
-    ./bitwarden.nix
-    ./torbox.nix
-    ./duckduckgo.nix
-    ./wayback-machine.nix
-    ./user-agent-switcher.nix
-    ./faster-pageload.nix
-    ./translate.nix
-    ./ublock-origin.nix
-    ./clearurls.nix
-    ./noptcha.nix
-    ./pwnfox.nix
-  ];
+  imports = importsList;
 }
