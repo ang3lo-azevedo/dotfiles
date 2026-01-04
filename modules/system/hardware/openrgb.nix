19268a7ca60793be0294429d72437f34ff39110b
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-04 22:50:31 +0000

    feat: update configuration for i2c support and remove OpenRGB module

--- modules/system/hardware/openrgb.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  services.hardware.openrgb.enable = true;
-  services.hardware.openrgb.package = pkgs.openrgb-with-all-plugins;
-
-  boot.kernelModules = [ "i2c-dev" "i2c-i801" ];
-}
