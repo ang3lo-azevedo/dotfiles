bd37c85dc43cfbdfc6539209f4501f43598d92e0
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-04 01:40:48 +0000

    feat: update zen-browser pins configuration; add Space and Masters pins, adjust University position

--- modules/home-manager/browsers/zen-browser/pins.nix
+++ modules/home-manager/browsers/zen-browser/pins.nix
@@ -1,9 +1,13 @@
 let
   spaces = {
+    "Space" = {
+      id = "b1a4f4e2-3c5d-4f7e-9f2a-8c9e4d5b6a7c";
+      position = 1000;
+    };
     "University" = {
       id = "44685729-10e1-4832-a869-0b3a93e2f165";
       icon = "🎓";
-      position = 1000;
+      position = 2000;
     };
   };
   pins = {
@@ -11,7 +15,15 @@ let
       id = "8b316d70-2b5e-4c46-bf42-f4e82d635154";
       url = "https://web.whatsapp.com/";
       isEssential = true;
-      position = 100;
+      position = 101;
+    };
+    "Masters" = {
+      id = "a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d";
+      workspace = spaces.University.id;
+      isGroup = true;
+      isFolderCollapsed = true;
+      editedTitle = true;
+      position = 102;
     };
   };
 in
