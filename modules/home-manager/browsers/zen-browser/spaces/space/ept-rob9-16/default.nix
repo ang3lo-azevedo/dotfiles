6b77d965e46a4d85f28b5d1a082549d8d9e15aac
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-05 14:10:20 +0000

    feat: enhance Zen Browser space configurations with sorting and order attributes

--- modules/home-manager/browsers/zen-browser/spaces/space/ept-rob9-16/default.nix
+++ modules/home-manager/browsers/zen-browser/spaces/space/ept-rob9-16/default.nix
@@ -8,6 +8,7 @@ in
       name = "EPT/ROB9-16";
       id = "a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d";
       workspace = spaceId;
+      order = 1;
       isGroup = true;
       isFolderCollapsed = true;
       editedTitle = true;
