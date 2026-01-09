2849521f3dcfd8b16302ec388e326647e3251f6c
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-09 10:07:22 +0000

    feat: update Zen Browser default applications to use beta version; modify code editor imports and settings for improved terminal functionality

--- modules/home-manager/media/mpv.nix
+++ modules/home-manager/media/mpv.nix
@@ -1,4 +1,15 @@
-{ pkgs, mpv-config, ... }:
+{ pkgs, lib, mpv-config, ... }:
+let
+  # Filter out .vscode directory from mpv-config source
+  filteredMpvConfig = lib.cleanSourceWith {
+    src = mpv-config;
+    filter = path: type:
+      let
+        name = baseNameOf (toString path);
+      in
+        name != ".vscode" && name != ".git" && name != ".gitignore";
+  };
+in
 {
   home.packages = [
     pkgs.mpv
@@ -6,8 +17,9 @@
   ];
 
   # MPV player configuration from external git repository
+  # Exclude .vscode directory as it's not part of mpv config
   xdg.configFile."mpv" = {
-    source = mpv-config;
+    source = filteredMpvConfig;
     recursive = true;
   };
 
