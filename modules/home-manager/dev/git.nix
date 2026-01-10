2ebff51f79babf59ed596a54c82850a258b15ac9
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-10 14:37:23 +0000

    feat: reorganize gaming configurations; remove vr.nix and integrate necessary imports; enhance git settings and update zsh aliases

---
+++ modules/home-manager/dev/git.nix
@@ -0,0 +1,13 @@
+{
+  programs.git = {
+    settings = {
+      user = {
+        name = "Angelo Azevedo";
+        email = "ang3lo@azevedos.eu.org";
+      };
+      init.defaultBranch = "main";
+    };
+    enable = true;
+    lfs.enable = true;
+  };
+}
