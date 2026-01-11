c1b35f72b668012fefee3d5bdaeaaf9b7c842d15
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-11 22:23:17 +0000

    feat: clean up Nix configurations by removing unused parameters and files

--- modules/system/gaming/vr/wivrn.nix
+++ modules/system/gaming/vr/wivrn.nix
@@ -12,6 +12,12 @@
     # Run WiVRn as a systemd service on startup
     autoStart = true;
 
+    package = pkgs.wivrn.overrideAttrs (oldAttrs: {
+      preFixup = ''
+        export U_PACING_COMP_MIN_TIME_MS=8
+      '';
+    });
+
     # If you're running this with an nVidia GPU and want to use GPU Encoding (and don't otherwise have CUDA enabled system wide), you need to override the cudaSupport variable.
     #package = (pkgs.wivrn.override { cudaSupport = true; });
 
