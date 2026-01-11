c1e3de432f7ea292d432b202e4848504a0963a9a
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-11 00:05:38 +0000

    feat: add lact service for VR gaming support; move lact configuration to gaming module and update imports

---
+++ modules/system/gaming/vr/lact.nix
@@ -0,0 +1,41 @@
+{
+  services.lact = {
+    enable = true;
+    settings = {
+      daemon = {
+        log_level = "info";
+        admin_group = "wheel";
+        disable_clocks_cleanup = false;
+      };
+      apply_settings_timer = 5;
+      profiles = {
+        "VR gaming" = {
+          gpus."1002:7590-1DA2:E493-0000:2f:00.0" = {
+            fan_control_enabled = false;
+            performance_level = "manual";
+            power_profile_mode_index = 4;
+          };
+          rule = {
+            type = "or";
+            filter = [
+              {
+                type = "process";
+                filter = {
+                  name = "wivrn-server";
+                };
+              }
+              {
+                type = "process";
+                filter = {
+                  name = "..envision-wrapped-wrapped";
+                };
+              }
+            ];
+          };
+        };
+      };
+      current_profile = "VR gaming";
+      auto_switch_profiles = true;
+    };
+  };
+}
