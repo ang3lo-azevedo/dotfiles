e16f9ea6390acdbef04ad02e472f9c6ffd52f165
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-11 11:31:02 +0000

    feat: add audio fix for Samsung Galaxy Book; create new module for hardware configuration

---
+++ modules/system/hardware/samsung-galaxy-book-audio.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.my.hardware.samsung-galaxy-book-audio;
+in
+{
+  options.hardware.samsung-galaxy-book-audio = {
+    enable = mkEnableOption "Enable audio fix for Samsung Galaxy Book";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.alsa-utils ];
+
+    systemd.services.samsung-galaxy-book-audio-fix = {
+      description = "Samsung Galaxy Book audio fix";
+      after = [ "sound.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      script = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/joshuagrisham/galaxy-book2-pro-linux/main/sound/necessary-verbs.sh";
+        sha256 = "1ix9s9bkycl7af4bpfw23wq30z9db8syhgch57k7rkp0rags6zdw";
+      };
+    };
+  };
+}
