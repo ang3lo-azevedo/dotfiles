d97b21635b521f6ba3156a5121fa95f32fa72028
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-12 22:53:11 +0000

    feat: reorganize Samsung Galaxy Book hardware modules; add audio and function key support

---
+++ modules/hardware/samsung-galaxy-book/fn_keys/scripts/kbdillumtoggle.sh
@@ -0,0 +1,8 @@
+DEVICE='samsung-galaxybook::kbd_backlight'
+CURRENT=$(brightnessctl --device="$DEVICE" g)
+MAX=$(brightnessctl --device="$DEVICE" m)
+if [ "$CURRENT" -ge "$MAX" ]; then
+brightnessctl --device="$DEVICE" set 0
+else
+brightnessctl --device="$DEVICE" set +1
+fi
