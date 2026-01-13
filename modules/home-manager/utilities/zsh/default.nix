845010b68b747635c1badf948490f1a4dfdd9880
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-13 19:36:34 +0000

    fix: update TODO.md with additional tasks and revise zsh update alias for improved clarity

--- modules/home-manager/utilities/zsh/default.nix
+++ modules/home-manager/utilities/zsh/default.nix
@@ -27,7 +27,7 @@
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake /home/ang3lo/nix-config";
       hmrebuild = "nix run home-manager -- switch --flake /home/ang3lo/nix-config#ang3lo";
-      update = "nix flake update --flake --accept-flake-config /home/ang3lo/nix-config";
+      update = "nix flake update --accept-flake-config --flake /home/ang3lo/nix-config";
       upgrade = "update && rebuild";
       
       ll = "eza -l";
