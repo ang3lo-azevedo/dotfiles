5184d1cfcdba38342c18303e6a7c5ad46a01643f
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-14 10:57:53 +0000

    feat: enhance eGPU and Thunderbolt handling with improved kernel parameters

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -8,4 +8,15 @@
     enable = true;
     enable32Bit = true;
   };
+
+  # Handle eGPU hot-unplug gracefully
+  boot.kernelParams = [
+    # Allow ACPI hotplug to work properly
+    "pci=pcie_bus_safe"
+    # Enable runtime PM for PCIe devices (helps with eGPU hot-unplug)
+    "amd_iommu=on"
+  ];
+
+  # Enable PCIe hotplug support
+  boot.kernelModules = [ "pcie_hp" "acpihp" ];
 }
