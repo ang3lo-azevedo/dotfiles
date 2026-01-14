4e74d825b66c1f530284b3c7075dd5c09ddfe597
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-14 16:37:57 +0000

    feat: add glaumar_repo input and update qrookie configuration for Qml support

--- modules/home-manager/gaming/vr/qrookie.nix
+++ modules/home-manager/gaming/vr/qrookie.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
+let
+  # Custom qcoro with Qml support enabled
+  qcoro-with-qml = pkgs.qt6Packages.qcoro.overrideAttrs (oldAttrs: {
+    cmakeFlags = [
+      "-DQCORO_WITH_QML=ON"
+      "-DBUILD_SHARED_LIBS=ON"
+    ];
+  });
+in
 {
   home.packages = with pkgs; [
-    glaumar_repo.qrookie
+    (glaumar_repo.qrookie.overrideAttrs (oldAttrs: {
+      buildInputs = builtins.map (dep: 
+        if dep.pname or "" == "qcoro" then qcoro-with-qml else dep
+      ) (oldAttrs.buildInputs or []) ++ [ qt6.qtdeclarative ];
+    }))
   ];
 }
