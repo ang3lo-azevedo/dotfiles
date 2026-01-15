bf3fe4cbdc68a6dc0a3858a621d64e6d15230bad
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-15 00:04:25 +0000

    feat: add wlxoverlay configuration for WayVR integration

--- modules/home-manager/gaming/vr/wayvr.nix
+++ modules/home-manager/gaming/vr/wayvr.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
+let
+  wlxoverlayDir = "../../../../home/ang3lo/.config/wlxoverlay";
+in
 {
   home.packages = with pkgs; [
     wayvr
   ];
+
+  xdg.config."wlxoverlay" = {
+    source = wlxoverlayDir;
+  };
 }
