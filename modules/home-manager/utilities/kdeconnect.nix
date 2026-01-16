c804716f5aed200151e7f8c0b13e65b911eeadb2
[REDACTED NAME] <[REDACTED EMAIL]>
2026-01-16 13:25:23 +0000

    feat: add kdeconnect service configuration and firewall rules

---
+++ modules/home-manager/utilities/kdeconnect.nix
@@ -0,0 +1,8 @@
+{
+  home-manager.users.username.services.kdeconnect.enable = true;
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+}
