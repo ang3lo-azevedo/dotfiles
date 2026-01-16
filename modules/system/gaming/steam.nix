2c2ab90e4b9e04dc4ab10da2078f2c8c95f07f2f
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-16 19:22:49 +0000

    feat: update flake.lock and flake.nix to include nixpkgs-extra, and enable protontricks in Steam configuration

--- modules/system/gaming/steam.nix
+++ modules/system/gaming/steam.nix
@@ -10,6 +10,8 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+    protontricks.enable = true;
   };
 
   # Enable Proton GE for better game compatibility
