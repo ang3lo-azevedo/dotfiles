2b55a18e8bd0c928c5b273c7116a9a6f47898483
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-01-16 01:40:34 +0000

    feat: add kaon-wrapped package with kvantum QML module support

--- modules/home-manager/gaming/vr/kaon.nix
+++ modules/home-manager/gaming/vr/kaon.nix
@@ -1,6 +1,25 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
+let
+  # Create a stub kvantum QML module
+  kvantum-stub = pkgs.runCommand "kvantum-qml-stub" {} ''
+    mkdir -p $out/lib/qt-6/qml/kvantum
+    cat > $out/lib/qt-6/qml/kvantum/qmldir << 'EOF'
+module kvantum
+EOF
+  '';
+  
+  kaon-wrapped = pkgs.symlinkJoin {
+    name = "kaon-wrapped";
+    paths = [ inputs.nixpkgs-xr.packages.${pkgs.stdenv.hostPlatform.system}.kaon ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/kaon \
+        --prefix QML2_IMPORT_PATH : ${kvantum-stub}/lib/qt-6/qml
+    '';
+  };
+in
 {
-  home.packages = with pkgs; [
-    kaon
+  home.packages = [
+    kaon-wrapped
   ];
 }
