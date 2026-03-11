4e6c41028f7e5002ce9df9325f9759ea1282c658
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-03-11 11:22:29 +0000

    feat: add burpsuite and processing modules, and update imports in default configurations

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -3,9 +3,11 @@
   imports = [
     ./python
     ./git.nix
+    ./intellij-idea.nix
     ./code-editors
     ./adb.nix
     ./latex.nix
+    ./processing.nix
   ];
 
   home.packages = with pkgs; [
