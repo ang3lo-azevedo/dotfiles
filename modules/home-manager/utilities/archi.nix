1dff13adf308e3488603f0409d6c9707e33ebc5a
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-04-28 13:06:57 +0100

    feat: update archi package definition to include version and source URL

--- modules/home-manager/utilities/archi.nix
+++ modules/home-manager/utilities/archi.nix
@@ -1,7 +1,8 @@
 { pkgs, inputs, ... }:
 let
-  # Import the archi package definition from the nixpkgs commit
+  lib = pkgs.lib;
   nixpkgsArchi = inputs.archi-nixpkgs;
+  version = "5.9.0";
   
   archiPackage = (import "${nixpkgsArchi}/pkgs/by-name/ar/archi/package.nix" {
     inherit (pkgs) lib stdenv fetchurl autoPatchelfHook makeWrapper jdk libsecret glib webkitgtk_4_1 wrapGAppsHook3 copyDesktopItems makeDesktopItem _7zz;
@@ -9,6 +10,13 @@ let
     # Provide nixosTests - normally from ${nixpkgs}/nixos/tests/archi.nix but not needed for home-manager
     nixosTests = { archi = null; };
   }).overrideAttrs (old: {
+    inherit version;
+    
+    src = pkgs.fetchurl {
+      url = "https://github.com/archimatetool/archi.io/releases/download/${version}/Archi-Linux64-${version}.tgz";
+      hash = "sha256-0/3/EZw5upB0dvyhS0sfKqp7C4tc6vGDW+O9WU5iTc8=";
+    };
+    
     passthru = (old.passthru or {}) // { 
       tests = { archi = null; };
       updateScript = null;
