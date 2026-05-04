75eaec9cc122c732e0938ef047839b89132f6149
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-05-04 14:16:51 +0100

    feat: update flake.lock and system configurations; rename steam-fetcher to steam-config-nix and adjust module paths for consistency

--- modules/system/gaming/default.nix
+++ modules/system/gaming/default.nix
@@ -5,11 +5,11 @@
     ./gamemode.nix
     ./vr
     inputs.steam-config-nix.nixosModules.default
-    inputs.nix-gaming.nixosModules.pipewire-low-latency
+    inputs.nix-gaming.nixosModules.pipewireLowLatency
     inputs.nix-gaming.nixosModules.platformOptimizations
   ];
 
-  # Enable Cachix for nix-gaming packages (optional but recommended)
+  # Enable Cachix for nix-gaming packages (binary cache for pre-built packages like wine)
   nix.settings = {
     substituters = [ "https://nix-gaming.cachix.org" ];
     trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
