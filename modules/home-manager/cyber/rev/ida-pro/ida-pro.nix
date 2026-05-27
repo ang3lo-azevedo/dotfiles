773fa5221dfb75dfcdcbc031362153ab880e6424
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-05-27 23:34:10 +0100

    feat: add ida-pro package configuration with build dependencies and post-install script

---
+++ modules/home-manager/cyber/rev/ida-pro/ida-pro.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+let
+  idaRun = ./ida93sp2/ida-pro_93_x64linux.run;
+  scriptJs = ./ida93sp2/kg_patch/keygen.js;
+in
+{
+  home.packages = with pkgs; [
+    (ida-pro.overrideAttrs (old: {
+      version = "9.3.0";
+      src = idaRun;
+
+      # Add Node.js as a build dependency so the 'node' command is available
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.nodejs ];
+      buildInputs = (old.buildInputs or []) ++ [ pkgs.libxcrypt-legacy ];
+
+      postInstall = (old.postInstall or "") + ''
+        if [ -d "$out/opt" ]; then
+          cp ${scriptJs} "$out/opt/script.js"
+          cd "$out/opt"
+          node ./script.js
+        fi
+      '';
+    }))
+  ];
+}
