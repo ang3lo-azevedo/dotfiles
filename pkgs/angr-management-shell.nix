1d020a5367ad1816d226251e4dfc77bff8d0ecfd
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-05-27 09:41:27 +0100

    feat: integrate angr-management package and update related configurations

--- pkgs/angr-management-shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.python313.withPackages (ps: with ps; [
-  setuptools-rust
-  rustc
-  cargo
-  angr-management
-])
--- pkgs/angr-management/default.nix
+++ pkgs/angr-management/default.nix
@@ -1,9 +1,28 @@
-{ pkgs }:
+{ stdenv, python3, lib, src ? null }:
 
-let
-  upstream = import (builtins.fetchGit {
-    url = "https://github.com/faukah/angr-management-nix";
-    ref = "main";
-  }) { inherit pkgs; };
-in
-upstream.angr-management
+stdenv.mkDerivation rec {
+  pname = "angr-management";
+  version = "unstable";
+
+  inherit src;
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/${pname}
+    cp -r . $out/share/${pname}/
+
+    cat > $out/bin/angr-management <<'EOT'
+#!/bin/sh
+exec ${python3}/bin/python3 -m angr_management "$@"
+EOT
+    chmod +x $out/bin/angr-management
+  '';
+
+  meta = with lib; {
+    description = "angr Management UI (sourced from GitHub)";
+    license = licenses.unfree; # upstream may have non-free parts
+    maintainers = with maintainers; [ ];
+  };
+}
