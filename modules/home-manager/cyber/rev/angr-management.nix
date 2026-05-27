9c90a025bbd102f8675a44749c8052ac52ee227a
[REDACTED NAME] <[REDACTED EMAIL]>
2026-05-27 11:28:42 +0100

    feat: wrap angr-management in a shell script for environment variable handling

--- modules/home-manager/cyber/rev/angr-management.nix
+++ modules/home-manager/cyber/rev/angr-management.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    angr-management
+    (writeShellScriptBin "angr-management" ''
+      exec env -u QT_STYLE_OVERRIDE -u QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE=Fusion ${angr-management}/bin/angr-management "$@"
+    '')
   ];
 }
