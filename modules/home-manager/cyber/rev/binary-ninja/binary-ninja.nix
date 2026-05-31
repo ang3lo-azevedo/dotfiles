306da5ba2dc4003c89a1ca413e85c8fa81a9b824
[REDACTED NAME] <[REDACTED EMAIL]>
2026-05-31 13:05:52 +0100

    feat: update .gitignore to exclude crack files; add binary-ninja post-install script for setup

--- modules/home-manager/cyber/rev/binary-ninja/binary-ninja.nix
+++ modules/home-manager/cyber/rev/binary-ninja/binary-ninja.nix
@@ -1,17 +1,33 @@
 { pkgs, ... }:
+let
+  binjaZip = ./binaryninja_linux_stable_personal.zip;
+  scriptPy = ./keygen.py;
+in
 {
   programs.binary-ninja = {
     enable = true;
-    # TODO: Remove this override once the upstream package is updated to work with the latest xorg server.
-    package = pkgs.binary-ninja-personal-wayland.override {
-      /*# Upstream package still references deprecated xorg.* aliases.
+    package = (pkgs.binary-ninja-personal-wayland.override {
+      overrideSource = binjaZip;
+
+      # TODO: Remove this override once the upstream package is updated to work with the latest xorg server.
       xorg = pkgs.xorg // {
         libXi = pkgs.libxi;
         libXrender = pkgs.libxrender;
         xcbutilimage = pkgs.libxcb-image;
         xcbutilrenderutil = pkgs.libxcb-render-util;
-      };*/
-      overrideSource = ./binja_release_8/binaryninja_personal_linux.zip;
-    };
+      };
+    }).overrideAttrs (old: {
+      # Add python3 to the build environment so we can execute the script
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.python3 ];
+
+      postInstall = (old.postInstall or "") + ''
+        # Binary Ninja typically installs into $out/opt/binaryninja
+        if [ -d "$out/opt/binaryninja" ]; then
+          cp ${scriptPy} "$out/opt/binaryninja/script.py"
+          cd "$out/opt/binaryninja"
+          python3 ./script.py
+        fi
+      '';
+    });
   };
 }
