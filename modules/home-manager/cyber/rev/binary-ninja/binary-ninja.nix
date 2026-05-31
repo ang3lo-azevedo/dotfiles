beb843adeb9477f694362f3cb42cc46fcec1752a
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-05-31 11:27:09 +0100

    feat: update .gitignore to include binaryninja; modify binary-ninja.nix for package source override

--- modules/home-manager/cyber/rev/binary-ninja/binary-ninja.nix
+++ modules/home-manager/cyber/rev/binary-ninja/binary-ninja.nix
@@ -4,13 +4,14 @@
     enable = true;
     # TODO: Remove this override once the upstream package is updated to work with the latest xorg server.
     package = pkgs.binary-ninja-personal-wayland.override {
-      # Upstream package still references deprecated xorg.* aliases.
+      /*# Upstream package still references deprecated xorg.* aliases.
       xorg = pkgs.xorg // {
         libXi = pkgs.libxi;
         libXrender = pkgs.libxrender;
         xcbutilimage = pkgs.libxcb-image;
         xcbutilrenderutil = pkgs.libxcb-render-util;
-      };
+      };*/
+      overrideSource = ./binja_release_8/binaryninja_personal_linux.zip;
     };
   };
 }
