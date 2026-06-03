e42a46aff608aed894277c8d299f0c32f467cf31
[REDACTED NAME] <[REDACTED EMAIL]>
2026-06-03 18:50:24 +0100

    refactor: disable vscode and add desktop entry for google-antigravity-ide

--- modules/home-manager/dev/code-editors/antigravity/default.nix
+++ modules/home-manager/dev/code-editors/antigravity/default.nix
@@ -8,4 +8,12 @@
   home.packages = with pkgs; [
     google-antigravity-ide
   ];
+
+  xdg.desktopEntries.google-antigravity-ide = {
+    name = "Google Antigravity IDE";
+    exec = "google-antigravity-ide-fhs";
+    icon = "${pkgs.google-antigravity-ide-no-fhs}/lib/google-antigravity-ide/resources/app/resources/linux/code.png";
+    categories = [ "Development" "IDE" ];
+    comment = "Google Antigravity IDE";
+  };
 }
--- modules/home-manager/dev/code-editors/default.nix
+++ modules/home-manager/dev/code-editors/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./vscode
+    # ./vscode
     ./antigravity
     #./cursor
   ];
