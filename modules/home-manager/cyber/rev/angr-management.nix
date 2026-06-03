182885e52aebff3d76656b7c378f85ac59b28198
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-06-03 18:32:16 +0100

    feat: add angr-management desktop entry, implement extension cleanup, and update ide settings activation strategy

--- modules/home-manager/cyber/rev/angr-management.nix
+++ modules/home-manager/cyber/rev/angr-management.nix
@@ -5,4 +5,12 @@
       exec env -u QT_STYLE_OVERRIDE -u QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE=Fusion ${angr-management}/bin/angr-management "$@"
     '')
   ];
+
+  xdg.desktopEntries.angr-management = {
+    name = "angr management";
+    exec = "angr-management";
+    icon = "application-x-executable";
+    categories = [ "Development" "Utility" ];
+    comment = "GUI for angr";
+  };
 }
