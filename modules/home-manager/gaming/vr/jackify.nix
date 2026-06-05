e2cde8b077c3191474e3ff2c46cdef906ac6f0e6
[REDACTED NAME] <[REDACTED EMAIL]>
2026-06-05 15:08:29 +0100

    chore: update flake inputs and refine package configurations for Caido, Steam, and system tools

--- modules/home-manager/gaming/vr/jackify.nix
+++ modules/home-manager/gaming/vr/jackify.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
+  # Jackify is currently unavailable because the upstream Mistyttm/nixpkgs-extra repo was deleted
   home.packages = with pkgs;[
-    jackify
+    # jackify
   ];
 }
