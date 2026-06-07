00a51aecf75d9b0d9774188890e633f339ac8c49
[REDACTED NAME] <[REDACTED EMAIL]>
2026-06-07 16:20:17 +0100

    chore: update flake inputs, configure webcam fixes, and add extensions to Zen Browser

--- modules/hardware/samsung-galaxy-book/webcam.nix
+++ modules/hardware/samsung-galaxy-book/webcam.nix
@@ -4,4 +4,9 @@
   imports = [
     "${inputs.samsung-galaxy-book-linux-fixes}/nixos/webcam-fix-book5.nix"
   ];
+
+  hardware.samsungGalaxyBook.webcamFixBook5 = {
+    videoFlip = true;
+    relayColorFilter = "videoflip method=rotate-180 ! videobalance hue=0.05 saturation=0.95";
+  };
 }
