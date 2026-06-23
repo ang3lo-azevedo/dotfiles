77994eeb2dc4ff63fc5efc6d422adf0f145a504f
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-06-23 16:08:30 +0100

    feat: implement swaync auto-reload service and update icon configuration

--- modules/home-manager/window-manager/services/swaync.nix
+++ modules/home-manager/window-manager/services/swaync.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 let
   mkSymlink = path: config.lib.file.mkOutOfStoreSymlink "/home/ang3lo/nix-config/home/ang3lo/.config/${path}";
 in
@@ -8,4 +8,20 @@ in
 
   xdg.configFile."swaync/config.json" = lib.mkForce { source = mkSymlink "swaync/config.json"; };
   xdg.configFile."swaync/style.css" = lib.mkForce { source = mkSymlink "swaync/style.css"; };
+
+  systemd.user.paths.swaync-config = {
+    Unit.Description = "Watch SwayNC configuration for changes";
+    Path.PathModified = [
+      "/home/ang3lo/nix-config/home/ang3lo/.config/swaync/config.json"
+      "/home/ang3lo/nix-config/home/ang3lo/.config/swaync/style.css"
+    ];
+    Path.Unit = "swaync-reload.service";
+    Install.WantedBy = [ "swaync.service" ];
+  };
+
+  systemd.user.services.swaync-reload = {
+    Unit.Description = "Reload SwayNC";
+    Service.Type = "oneshot";
+    Service.ExecStart = "${pkgs.bash}/bin/bash -c '${config.services.swaync.package}/bin/swaync-client -R && ${config.services.swaync.package}/bin/swaync-client -rs'";
+  };
 }
