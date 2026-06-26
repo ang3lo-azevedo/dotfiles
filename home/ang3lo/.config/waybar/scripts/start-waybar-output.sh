a80a2c6ec18933ccb2f48663fe8cb7fe100efc7c
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-06-26 16:43:39 +0100

    feat: update waybar startup script to toggle external monitor visibility and update flake inputs

--- home/ang3lo/.config/waybar/scripts/start-waybar-output.sh
+++ home/ang3lo/.config/waybar/scripts/start-waybar-output.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Start a waybar instance for the given output, e.g. eDP-1 or DP-3.
 # Usage: start-waybar-output.sh <OUTPUT_NAME>
 OUTPUT="$1"
@@ -7,4 +7,19 @@ TEMP_CONFIG="/tmp/waybar-${OUTPUT}.jsonc"
 sed "s/EXTERNAL_OUTPUT/${OUTPUT}/g" \
 	~/.config/waybar/config.jsonc >"$TEMP_CONFIG"
 
-exec waybar -c "$TEMP_CONFIG" -s ~/.config/waybar/style.css
+waybar -c "$TEMP_CONFIG" -s ~/.config/waybar/style.css &
+WAYBAR_PID=$!
+
+# External monitors start hidden: the trigger bar is used to reveal them.
+# eDP outputs are the built-in laptop screen and start expanded.
+case "$OUTPUT" in
+eDP*) ;;
+*)
+	sleep 1
+	kill -SIGUSR1 $WAYBAR_PID
+	;;
+esac
+
+# Forward SIGTERM so systemd can stop the service cleanly
+trap 'kill $WAYBAR_PID' TERM INT
+wait $WAYBAR_PID
