d609781cb69bc33c3dbc9081270f42987659fab0
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-23 23:43:27 +0100

    fix(aliases): add ?submodules=1 to rebuild aliases to ensure local ang3lo-nur and private submodules are evaluated

--- modules/home-manager/utilities/zsh/shell-aliases.nix
+++ modules/home-manager/utilities/zsh/shell-aliases.nix
@@ -25,8 +25,8 @@ in {
 
     # NixOS related aliases
     fmt = "(cd ~/nix-config && pre-commit run --all-files)";
-    rebuild = "sudo -v && git -C ~/nix-config add -N . 2>/dev/null; fmt || true; sudo nixos-rebuild switch --accept-flake-config --impure --flake ~/nix-config#pc-angelo -L --keep-going";
-    hmrebuild = "git -C ~/nix-config add -N . 2>/dev/null; fmt || true; home-manager switch --accept-flake-config --impure --flake ~/nix-config#ang3lo";
+    rebuild = "sudo -v && git -C ~/nix-config add -N . 2>/dev/null; fmt || true; sudo nixos-rebuild switch --accept-flake-config --impure --flake 'git+file:///home/ang3lo/nix-config?submodules=1#pc-angelo' -L --keep-going";
+    hmrebuild = "git -C ~/nix-config add -N . 2>/dev/null; fmt || true; home-manager switch --accept-flake-config --impure --flake 'git+file:///home/ang3lo/nix-config?submodules=1#ang3lo'";
     nvfetcher = "nvfetcher -c ~/nix-config/pkgs/nvfetcher.toml -o ~/nix-config/pkgs/_sources $([ -f ${keyfile} ] && echo \"-k ${keyfile}\") && if [ -d ~/nur-packages ]; then nvfetcher -c ~/nur-packages/nvfetcher.toml -o ~/nur-packages/_sources $([ -f ${keyfile} ] && echo \"-k ${keyfile}\"); fi";
     update = "(cd ~/nix-config && nvfetcher && nix flake update --accept-flake-config)";
     upgrade = "sudo -v && git -C ~/nix-config pull && nvfetcher && rebuild";
