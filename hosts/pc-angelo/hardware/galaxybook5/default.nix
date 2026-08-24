798a3716e92617e25030289eaef0df4f36d055e9
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-24 10:52:26 +0100

    fix(acpi): silence IETM ACPI errors and improve eGPU stability with kernel params

--- hosts/pc-angelo/hardware/galaxybook5/default.nix
+++ hosts/pc-angelo/hardware/galaxybook5/default.nix
@@ -34,7 +34,7 @@
     };
 
     # Add kernel parameter to force Xen to probe all devices, and silence ACPI errors
-    kernelParams = ["xe.force_probe=*" "quiet" "loglevel=3" "systemd.show_status=auto" "rd.udev.log_level=3"];
+    kernelParams = ["xe.force_probe=*" "quiet" "loglevel=3" "systemd.show_status=auto" "rd.udev.log_level=3" "acpi_osi=!Windows" "acpi_osi=Linux" "acpi=noirq"];
     consoleLogLevel = 0;
   };
 
