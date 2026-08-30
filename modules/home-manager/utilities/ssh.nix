eca3e24b3287ed41cdcaa89768e8f71af867bb8c
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-30 22:49:26 +0100

    refactor: update gitlab identity file and add activation script to fix ssh permissions issue

--- modules/home-manager/utilities/ssh.nix
+++ modules/home-manager/utilities/ssh.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   programs.ssh = {
     enable = true;
 
@@ -43,8 +43,7 @@
       "gitlab-syssec.dpss.inesc-id.pt" = {
         host = "gitlab-syssec.dpss.inesc-id.pt 146.193.41.153";
         user = "git";
-        identityFile = "~/.ssh/gitlab_ist";
-        identitiesOnly = true;
+        identityFile = "~/.ssh/dpss-inesc";
       };
 
       "github.com" = {
@@ -64,4 +63,16 @@
       };
     };
   };
+
+  # OpenSSH "Bad owner or permissions" Workaround
+  # Home Manager usually creates ~/.ssh/config as a symlink to the Nix store.
+  # When using remote builders or certain containers, the store path may be owned by `nobody`.
+  # OpenSSH strictly rejects config files that aren't owned by your user or root.
+  # This trick forces Home Manager to symlink to a backup file (.ssh/config_hm),
+  # and then copies it to a real, properly-owned file at .ssh/config with strict 600 permissions.
+  home.file.".ssh/config".target = lib.mkForce ".ssh/config_hm";
+  home.activation.copySshConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    cp -f ~/.ssh/config_hm ~/.ssh/config
+    chmod 600 ~/.ssh/config
+  '';
 }
