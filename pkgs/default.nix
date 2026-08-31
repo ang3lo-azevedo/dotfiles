866c0642a5629d2b3288910489f0b72688cd4ded
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-31 17:57:50 +0100

    Expose vorion directly in pkgs overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -21,6 +21,8 @@
       then /home/ang3lo/Documents/projects/ist-fenix-auto-enroller
       else inputs.ist-fenix-auto-enroller;
   };
+
+  vorion = pkgs.callPackage ./ang3lo-nur/pkgs/vorion {};
 }
 // (
   if inputs ? ang3lo-nur
